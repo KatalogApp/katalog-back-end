24c855c2d6437daa5d775e4f18ecc3ee0a8a011a
Ale Bausa <alebausa@macbook-pro-de-ale.home>
2021-07-15 21:09:57 +0200

    fixes User model

--- models/User.js
+++ models/User.js
@@ -4,40 +4,33 @@ const { Schema } = mongoose;
 const userSchema = new Schema(
 	{
 		name: {
-        type: String,
-        trim: true,
-        required: [true, 'name is required.'],
-        unique: true
-      },
-		
-			
-      email: {
-        type: String,
-        required: [true, 'Email is required.'],
-        match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address.'],
-        unique: true,
-        lowercase: true,
-        trim: true
-			},
-			
-			
-      hashedPassword: {
-        type: String,
-        required: [true, 'Password is required.']
-      },
-  
-    
-      posts: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
-	  
-	  
-      timestamps: {
+			type: String,
+			trim: true,
+			required: [true, 'name is required.'],
+			unique: true,
+		},
+		email: {
+			type: String,
+			required: [true, 'Email is required.'],
+			match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address.'],
+			unique: true,
+			lowercase: true,
+			trim: true,
+		},
+		hashedPassword: {
+			type: String,
+			required: [true, 'Password is required.'],
+		},
+		posts: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
+	},
+	{
+		timestamps: {
 			createdAt: 'created_at',
 			updatedAt: 'updated_at',
-		  }
-    
+		},
+	}
 );
 
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
-
