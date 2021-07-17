ea26aa3a3fad06850f415c870057856db3b3437b
Brian Dowd <dowdbrian@yahoo.com>
2021-07-17 14:20:18 +0200

    posts routes done

--- routes/posts.js
+++ routes/posts.js
@@ -11,16 +11,10 @@ const router = new Router();
 // eslint-disable-next-line import/no-unresolved
 // const fileUploader = require('../configs/cloudinary.config');
 
-// RENDER CREATE A POST PAGE
-// YOU DON'T NEED THIS ONE
-router.get('/user-profile/create', (req, res) => {
-	res.status(200).json({ create: 'a post here' });
-	//console.log("hello")
-});
-
 // CREATE NEW POST
+
 router.post('/user-profile/create', async (req, res) => {
-// router.post('/user-profile/create', fileUploader.single('image'), async (req, res) => {
+	// router.post('/user-profile/create', fileUploader.single('image'), async (req, res) => {
 	const { title, date, description, keywords, theme, creator } = req.body;
 	// eslint-disable-next-line no-underscore-dangle
 	const userId = req.session.currentUser._id;
@@ -36,35 +30,26 @@ router.post('/user-profile/create', async (req, res) => {
 	} finally {
 		const updatedUser = await User.findById(userId).populate({ path: 'posts', model: Post });
 		req.session.currentUser = updatedUser;
-		res.status(200).json({ user: updatedUser });
+		res.status(201).json({ user: updatedUser });
 	}
 });
 
-// GET update post
-// YOU DON'T NEED THIS
-router.get('/user-profile/:id/edit', (req, res, next) => {
-	// const postId = req.session.currentUser.posts[0]._id;
-	const postId = req.params.id;
-	Post.findById(postId)
-		.then(postToEdit => {
-			// eslint-disable-next-line no-console
-			console.log(postToEdit);
-			return res.render('edit-post', { post: postToEdit });
-		})
-		.catch(error => next(error));
-});
-
 // POST update post
-router.post('/user-profile/:id/edit', async (req, res, next) => {
+
+router.post('/user-profile/:id/edit', async (req, res) => {
 	// const postId = req.session.currentUser.posts._id;
 	const postId = req.params.id;
-	const { title, description, theme } = req.body;
+	const { title, date, description, keywords, theme, creator } = req.body;
 	try {
-		const newPost = await Post.findByIdAndUpdate(postId, { title, description, theme }, { new: true });
+		const newPost = await Post.findByIdAndUpdate(
+			postId,
+			{ title, date, description, keywords, theme, creator },
+			{ new: true }
+		);
 		if (!newPost) {
 			console.log('No post created');
 		}
-		res.status(201).json(newPost);
+		res.status(200).json(newPost);
 	} catch (error) {
 		console.log(error);
 	}
@@ -72,13 +57,14 @@ router.post('/user-profile/:id/edit', async (req, res, next) => {
 
 // Delete post
 
-router.post('/user-profile/:id/delete', (req, res) => {
+router.delete('/user-profile/:id/delete', async (req, res) => {
 	const postId = req.params.id;
-	Post.findByIdAndDelete(postId)
-	// QUITAR REDIRECT
-		.then(() => res.redirect('/user-profile'))
-		// eslint-disable-next-line no-undef
-		.catch(error => next(error));
+	try {
+		const deletedPost = await Post.findByIdAndDelete(postId);
+		res.status(200).json(deletedPost);
+	} catch (error) {
+		console.log(error, 'error occurred when deleting post');
+	}
 });
 
 module.exports = router;
