b5bb2049fc9220c44eb0af8c9b9dc5749f8a504f
Ale Bausa <alebausa@macbook-pro-de-ale.home>
2021-07-26 20:22:01 +0200

    Creates 2 new routes in posts

--- routes/posts.js
+++ routes/posts.js
@@ -11,16 +11,36 @@ const router = new Router();
 // eslint-disable-next-line import/no-unresolved
 // const fileUploader = require('../configs/cloudinary.config');
 
-// CREATE NEW POST
-
-router.post('/user-profile/post/create', async (req, res) => {
-	// router.post('/user-profile/create', fileUploader.single('image'), async (req, res) => {
-	const { title, date, description, keywords, theme, creator } = req.body;
+// GET POSTS
+router.get('/user-profile/posts', async (req, res) => {
 	// eslint-disable-next-line no-underscore-dangle
 	const userId = req.session.currentUser._id;
-	// const file = req.file.path;
+	if (!req.session.currentUser) {
+		res.status(401).json({ status: 'No user logged in' });
+	}
+	const user = await User.findById(userId).populate('posts');
+	res.status(200).json(user);
+});
+
+// GET SINGLE POST
+router.get('/user-profile/posts/:id', async (req, res) => {
+	// eslint-disable-next-line no-underscore-dangle
+	const postId = req.params.id;
+	const post = await Post.findById(postId);
+	if (!post) {
+		res.status(404).json({ status: 'No post found' });
+	}
+	res.status(200).json(post);
+});
+
+// CREATE NEW POST
+router.post('/user-profile/post/create', async (req, res) => {
+	// router.post('/user-profile/create', fileUploader.single('image'), async (req, res) => {
+	const { title, description, keywords, theme } = req.body;
+	// eslint-disable-next-line no-underscore-dangle
+	const userId = req.session.currentUser._id;
 	try {
-		const newPost = await Post.create({ userId, title, date, description, keywords, theme, creator });
+		const newPost = await Post.create({ userId, title, description, keywords, theme, creator: userId });
 		const currentUser = await User.findById(userId);
 		// eslint-disable-next-line no-underscore-dangle
 		currentUser.posts.push(newPost._id);
